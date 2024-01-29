613139e523ccf33ff884028cd15445f88b38091a
kenji <kenji@lynx.eco>
2024-01-28 21:51:55 -0800

    Initial Commit

---
+++ visionML/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  visionML
+//
+//  Created by user on 1/28/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ visionML/visionMLApp.swift
@@ -0,0 +1,17 @@
+//
+//  visionMLApp.swift
+//  visionML
+//
+//  Created by user on 1/28/24.
+//
+
+import SwiftUI
+
+@main
+struct visionMLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
